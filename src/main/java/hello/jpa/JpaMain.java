85b7876beeaa48fff5beb92f5d57f10532af4deb
tmxhsk99 <tmxhsk99@naver.com>
2022-07-07 10:04:18 +0900

    feat : 양 방향 매핑 순수한 객체 관계를 고려 예제 추가

--- src/main/java/hello/jpa/JpaMain.java
+++ src/main/java/hello/jpa/JpaMain.java
@@ -21,29 +21,28 @@ public class JpaMain {
             team.setName("TeamA");
             em.persist(team);
 
+            //1차캐시에 Team만 등록됨 | 1L | TeamA |
+
             Member member = new Member();
             member.setName("member1");
             member.setTeam(team);
             em.persist(member);
 
+            //1차캐시에서 Member 등록함 | 1L | member1 | TeamA |
+
             //1차 캐시에서 가져오기 때문에 영속성 콘텍스트를 초기야 해야 쿼리를 볼수 있다.
-            em.flush();
-            em.clear();
+//            em.flush();
+//            em.clear();
 
-            Member findMember = em.find(Member.class,member.getId());
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
-            // 양방향 연결 셋팅 후 start
-            List<Member> members = findMember.getTeam().getMembers();
-
+            Team findTeam = em.find(Team.class,team.getId()); //1차 캐시에 있는 Team 객체를 가져옴 , 그러나 아직 쿼리가 안날라갔기때문에 Members에 객체가 셋팅이 안되어있는 상태
+            List<Member> members = findTeam.getMembers();
+            System.out.println("==========================");
+            //쿼리문으로 연관관계가 맵핑되지 않은 상태인 순수 객체이므로 멤버가 나오지 않는다.
             for (Member m : members) {
                 System.out.println("m = " + m.getName());
             }
-            // 양방향 연결 셋팅 후 End
+            System.out.println("==========================");
 
-            //이런식으로 하면 fk가 업데이트 된다. (100번 ID를 있다 치고)
-//            Team newTeam = em.find(Team.class, 100L);
-//            findMember.setTeam(newTeam);
 
             tx.commit();
         } catch (Exception e) {
