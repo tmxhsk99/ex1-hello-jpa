2cbfc790f08c177ea1604e82c412a3652da5d209
tmxhsk99 <tmxhsk99@naver.com>
2022-07-07 12:07:16 +0900

    feat : 연관관계 편의 메서드 적용

--- src/main/java/hello/jpa/JpaMain.java
+++ src/main/java/hello/jpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.Arrays;
 import java.util.List;
 
 public class JpaMain {
@@ -25,19 +24,18 @@ public class JpaMain {
 
             Member member = new Member();
             member.setName("member1");
-            member.setTeam(team);
+            //연관관계 편의 메서드 적용시
+            member.changeTeam(team);
             em.persist(member);
 
-            //1차캐시에서 Member 등록함 | 1L | member1 | TeamA |
 
             //1차 캐시에서 가져오기 때문에 영속성 콘텍스트를 초기야 해야 쿼리를 볼수 있다.
 //            em.flush();
 //            em.clear();
 
-            Team findTeam = em.find(Team.class,team.getId()); //1차 캐시에 있는 Team 객체를 가져옴 , 그러나 아직 쿼리가 안날라갔기때문에 Members에 객체가 셋팅이 안되어있는 상태
+            Team findTeam = em.find(Team.class,team.getId());
             List<Member> members = findTeam.getMembers();
             System.out.println("==========================");
-            //쿼리문으로 연관관계가 맵핑되지 않은 상태인 순수 객체이므로 멤버가 나오지 않는다.
             for (Member m : members) {
                 System.out.println("m = " + m.getName());
             }
--- src/main/java/hello/jpa/Member.java
+++ src/main/java/hello/jpa/Member.java
@@ -1,19 +1,11 @@
 package hello.jpa;
 
-import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @Entity
 public class Member {
@@ -52,8 +44,9 @@ public class Member {
         return team;
     }
 
-    public void setTeam(Team team) {
+    public void changeTeam(Team team) {
         this.team = team;
+        team.getMembers().add(this);
     }
 }
 
