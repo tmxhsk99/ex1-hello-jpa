18ecb430657dcabb8a01a9b66bcba3b0c30854b5
tmxhsk99 <tmxhsk99@naver.com>
2022-07-19 08:59:11 +0900

    feat : 프록시 확인 및 하이버네이트 강제 초기화 예제

--- src/main/java/hello/jpa/JpaMain.java
+++ src/main/java/hello/jpa/JpaMain.java
@@ -7,6 +7,8 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.List;
+import javax.persistence.PersistenceUtil;
+import org.hibernate.Hibernate;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -22,11 +24,14 @@ public class JpaMain {
             em.persist(member);
             em.flush();
             em.clear();
-            //Member findMember = em.find(Member.class, member.getId());
-            Member findMember = em.getReference(Member.class, member.getId());
-            System.out.println("findMember.getClass() = " + findMember.getClass());
-            System.out.println("findMember = " + findMember.getId());
-            //System.out.println("findMember = " + findMember.getUsername());
+
+            Member refMember = em.getReference(Member.class, member.getId());
+            System.out.println("refMember = " + refMember.getClass()); //Proxy
+            //refMember.getUsername();
+            //프록시 인스턴스의 초기화 여부 확인
+            //System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
+            //강제 초기화
+            Hibernate.initialize(refMember);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
