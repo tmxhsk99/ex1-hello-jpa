42459ba922115d08f53d05e73aa1722c3bb1280a
tmxhsk99 <tmxhsk99@naver.com>
2022-07-19 12:50:55 +0900

    feat : 즉시로딩, 지연로딩 관련 예제 추가

--- src/main/java/hello/jpa/JpaMain.java
+++ src/main/java/hello/jpa/JpaMain.java
@@ -19,22 +19,31 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member();
-            member.setUsername("hello");
-            em.persist(member);
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setTeam(team);
+
+            em.persist(member1);
+
+
             em.flush();
             em.clear();
 
-            Member refMember = em.getReference(Member.class, member.getId());
-            System.out.println("refMember = " + refMember.getClass()); //Proxy
-            //refMember.getUsername();
-            //프록시 인스턴스의 초기화 여부 확인
-            //System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
-            //강제 초기화
-            Hibernate.initialize(refMember);
+            Member m = em.find(Member.class, member1.getId());
+
+            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
+            System.out.println("===========");
+            m.getTeam().getName();
+            System.out.println("===========");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             //close
             em.close();
--- src/main/java/hello/jpa/Member.java
+++ src/main/java/hello/jpa/Member.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -12,15 +13,15 @@ import javax.persistence.OneToOne;
 
 @Entity
 public class Member extends  BaseEntity{
-
     @Id @GeneratedValue
     @Column(name="MEMBER_ID")
     private Long id;
     @Column(name = "USER_NAME")
     private String username;
 
-    @ManyToOne //일 대 다 양방향 , 읽기 전용이 되어버림
-    @JoinColumn(name="TEAM_ID" ,insertable = false,updatable = false)
+    //@ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="TEAM_ID")
     private Team team;
 
 
