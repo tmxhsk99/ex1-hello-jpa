bc30744edca2f54a414dd61d479de1bff8ecde48
tmxhsk99 <tmxhsk99@naver.com>
2022-07-20 12:06:38 +0900

    feat : 값 타입과 불변객체 관련 예제 추가

--- src/main/java/hello/jpa/Member.java
+++ src/main/java/hello/jpa/Member.java
@@ -30,17 +30,6 @@ public class Member {
     @Embedded
     private Address homeAddress;
 
-    @Embedded
-    @AttributeOverrides({
-        @AttributeOverride(
-            name ="city", column = @Column(name = "WORK_CITY")),
-        @AttributeOverride(
-            name = "street", column = @Column(name = "WORK_STREET")),
-        @AttributeOverride(
-            name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
-    })
-    private Address workAddress;
-
     public Long getId() {
         return id;
     }
@@ -73,12 +62,5 @@ public class Member {
         this.homeAddress = homeAddress;
     }
 
-    public Address getWorkAddress() {
-        return workAddress;
-    }
-
-    public void setWorkAddress(Address workAddress) {
-        this.workAddress = workAddress;
-    }
 }
 
--- src/main/java/hello/jpa/Period.java
+++ src/main/java/hello/jpa/Period.java
@@ -30,15 +30,9 @@ public class Period {
         return startDate;
     }
 
-    public void setStartDate(LocalDateTime startDate) {
-        this.startDate = startDate;
-    }
 
     public LocalDateTime getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(LocalDateTime endDate) {
-        this.endDate = endDate;
-    }
 }
