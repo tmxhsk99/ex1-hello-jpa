31d68b8b444f7999a24888be15441f36b999952b
tmxhsk99 <tmxhsk99@naver.com>
2022-07-20 14:24:42 +0900

    feat : 값타입 수정 예제 추가

--- src/main/java/hello/jpa/JpaMain.java
+++ src/main/java/hello/jpa/JpaMain.java
@@ -36,6 +36,21 @@ public class JpaMain {
             em.clear();
             System.out.println("=================== START ==================");
             Member findMember = em.find(Member.class, member.getId());
+            //값타입 수정
+            //homeCity -> newCity;
+            //안 좋은 예 : 사이드 이펙트가 발생할 수 있음
+            //findMember.getHomeAddress().setCity("newCity");
+            //좋은 예
+            Address address = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity",address.getStreet(),address.getZipcode()));
+
+            //치킨 -> 한식
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+            //Address의 Equals 를 제대로 구현해야한다.
+            findMember.getAddressHistory().remove(new Address("old1","street","10000"));
+            findMember.getAddressHistory().add(new Address("newCity","street","10000"));
 
             tx.commit();
         } catch (Exception e) {
